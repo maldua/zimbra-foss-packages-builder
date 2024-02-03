2959a9c174247c59ec4d9a8f6b52bea7df1719c1
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2024-02-03 16:18:51 +0100

    packages-builder.sh: Install build dependencies as needed.

--- packages-builder.sh
+++ packages-builder.sh
@@ -10,8 +10,22 @@ do
         comp_safe=$(echo $comp | sed -e 's~/~_~g')
         make_all_log_file="make_all_${build_number_string}_${comp_safe}.txt"
         dpkg_scanpackages_log_file="dpkg_scanpackages_${build_number_string}_${comp_safe}.txt"
+        build_dep_log_file="build_dep_${build_number_string}_${comp_safe}.txt"
         cd $comp
         echo "${build_number_string} . Processing... '${comp}'"
+
+        # Install build dependencies as needed
+        package_dir=$(ls -d */ | grep -e '^zimbra-')
+        if sudo apt-get -qq build-dep $(pwd)/${package_dir} -y > ${build_pwd}/${build_dep_log_file} 2>&1 ; then
+            :
+        else
+          echo "ERROR. ${comp} failed. Please check: '${build_dep_log_file}' inside 'packages'."
+          cd "${build_pwd}"
+          cd ..
+          exit 1
+        fi
+
+        # Actual build
         if make all > ${build_pwd}/${make_all_log_file} 2>&1 ; then
           cp build/UBUNTU20_64/*deb /var/local/repo
           cd /var/local/repo && dpkg-scanpackages . /dev/null 2> ${build_pwd}/${dpkg_scanpackages_log_file} | gzip -9c > Packages.gz
